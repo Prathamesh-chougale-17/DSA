f9e30b5d9b45a5bb941e7f7151cda35538a201ac
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2022-11-28 10:53:35 +0530

    saving

--- assignment-3.cpp
+++ assignment-3.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{
+{   stack <int> st;
+    st.push(2);
+    while (!st.empty())
+    {
+cout<<st.top()<<"\n";
+    st.pop()
+    }
+    cout<<"prathamesh";
  return 0;
 }
